8f54d345d0fd6354dcd448c200c8a7fdc1badee8
ryabkov <gold_100@bk.ru>
2019-02-12 17:54:15 +0300

    page catalog

---
+++ app/js/catalog.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(function () {
+
+	/*______ Catalog custom select ______*/
+
+	$.each($('.js__custom-select'), function (i, el) {
+		$(el).select2({
+			minimumResultsForSearch: -1,
+			dropdownPosition: 'below',
+			theme: $(el).attr('theme')
+		});
+	});
+
+});
