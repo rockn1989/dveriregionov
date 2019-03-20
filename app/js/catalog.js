9818a45750e4ab3177fbfe95ec9d5180b2a201f7
ryabkov <gold_100@bk.ru>
2019-03-20 17:52:03 +0300

    css fix

--- app/js/catalog.js
+++ app/js/catalog.js
@@ -18,4 +18,23 @@ $(function () {
 		$('.mobile-filter').slideToggle('350');
 	})
 
+	var cardHeight = [];
+
+	$('.catalog-body .card .card-wrapper').each(function (i, el) {
+		return cardHeight.push($(el).outerHeight());
+	});
+
+	cardHeight.sort(function(a, b) {
+		return b - a;
+	});
+
+	$('.catalog-body .card .card-wrapper').map(function (i, el) {
+		$(el).css('minHeight', cardHeight[0] + 'px')
+	});
+
+	$.each($('.catalog-body .card'), function (i, el) {
+		$(el).css('height',$(el).find('.card-wrapper').outerHeight()+'px');
+	});
+
+
 });
